5457a107de0b9b12988cf6e4bd46296b256ab659
Pawel Chmiel <pawel.chmiel@droidsonroids.pl>
2016-09-27 11:47:19 +0200

    Code cleanup

--- Notifications/ViewController.swift
+++ Notifications/ViewController.swift
@@ -13,6 +13,8 @@ import UserNotifications
 class ViewController: UIViewController {
     
     static let dontTapActionIdentifier = "pleaseDontTap"
+    static let categoryIdentifier = "category"
+    static let myNotificationIdentifier = "myNotification"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,14 +28,14 @@ class ViewController: UIViewController {
         
         // 0) Create and register an action and cateogry
         let action = UNNotificationAction(identifier: ViewController.dontTapActionIdentifier, title: "DON'T TAP ME", options: .destructive)
-        let category = UNNotificationCategory(identifier: "category", actions: [action], intentIdentifiers: [], options: .customDismissAction)
+        let category = UNNotificationCategory(identifier: ViewController.categoryIdentifier, actions: [action], intentIdentifiers: [], options: .customDismissAction)
         UNUserNotificationCenter.current().setNotificationCategories([category])
         
         // 1) Create a notification content
         let notificationContent = UNMutableNotificationContent()
         notificationContent.title = "Droids On Roids"
         notificationContent.body = "Notification"
-        notificationContent.categoryIdentifier = "category"
+        notificationContent.categoryIdentifier = ViewController.categoryIdentifier
         
         // 2) Create a DateComponents for every o'clock hour
         var dateComponents = DateComponents()
@@ -43,7 +45,7 @@ class ViewController: UIViewController {
         let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
 
         // 4) Create a request with given identifier content and trigger
-        let request = UNNotificationRequest(identifier: "myNotification", content: notificationContent, trigger: trigger)
+        let request = UNNotificationRequest(identifier: ViewController.myNotificationIdentifier, content: notificationContent, trigger: trigger)
         
         // 5) Add the request to the UserNotificationCenter
         UNUserNotificationCenter.current().add(request) { (error) in
